b7a1548b18fe9fa9ae464ea84654fb12f08a7b58
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2022-09-11 11:31:26 +0200

    utils

--- usr/lib/enigma2/python/Plugins/Extensions/tvaddon/Update.py
+++ usr/lib/enigma2/python/Plugins/Extensions/tvaddon/Update.py
@@ -4,7 +4,6 @@ PY3 = sys.version_info.major >= 3
 print("Update.py")
 
 def upd_done():        
-    from twisted.web.client import downloadPage
     print( "In upd_done")
     xfile ='http://patbuweb.com/tvPanel/tvaddon.tar'
     print('xfile: ', xfile)
