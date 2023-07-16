a67123f0df7b20079b2b23a165c5fb89e9f3a5e5
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2023-07-16 11:42:23 +0200

    Update Update.py

--- usr/lib/enigma2/python/Plugins/Extensions/tvaddon/Update.py
+++ usr/lib/enigma2/python/Plugins/Extensions/tvaddon/Update.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import sys
+import sys, os
 PY3 = sys.version_info.major >= 3
 print("Update.py")
 
@@ -9,11 +9,14 @@ print("Update.py")
 def upd_done():
     from os import popen, system
     cmd01 = "wget http://patbuweb.com/tvPanel/tvaddon.tar -O /tmp/tvaddon.tar ; tar -xvf /tmp/tvaddon.tar -C /"
-    cmd02 = "wget --no-check-certificate -U 'Enigma2 - TvAddon Plugin' -c 'http://patbuweb.com/tvPanel/tvaddon.tar' -O '/tmp/tvaddon.tar'; tar -xvf /tmp/tvaddon.tar -C /"
+    cmd02 = "wget --no-check-certificate -U 'Enigma2 - TvAddon Plugin' -c 'https://patbuweb.com/tvPanel/tvaddon.tar' -O '/tmp/tvaddon.tar'; tar -xvf /tmp/tvaddon.tar -C /"
     cmd22 = 'find /usr/bin -name "wget"'
     res = popen(cmd22).read()
     if 'wget' not in res.lower():
-        cmd23 = 'apt-get update && apt-get install wget'
+        if os.path.exists('/etc/opkg')
+            cmd23 = 'opkg update && opkg install wget'
+        else:
+            cmd23 = 'apt-get update && apt-get install wget'
         popen(cmd23)
     try:
         popen(cmd02)
