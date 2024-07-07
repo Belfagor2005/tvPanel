cd4cc4d5476574c66a2f35a34bbf561adfe455b0
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2024-07-07 15:34:47 +0200

    Update Update.py

--- usr/lib/enigma2/python/Plugins/Extensions/tvaddon/Update.py
+++ usr/lib/enigma2/python/Plugins/Extensions/tvaddon/Update.py
@@ -11,7 +11,7 @@ def upd_done():
     from os import popen, system
     installUrl = 'https://raw.githubusercontent.com/Belfagor2005/tvPanel/main/installer.sh'
     cmd00 = 'wget -q "--no-check-certificate" ' + installUrl + ' -O - | /bin/sh'
-
+    popen(cmd00)
     # cmd01 = "wget --no-cache --no-dns-cache http://patbuweb.com/tvPanelxxx/tvaddon.tar -O /tmp/tvaddon.tar --post-data='action=purge';tar -xvf /tmp/tvaddon.tar -C /"
     # cmd02 = "wget --no-check-certificate --no-cache --no-dns-cache -U 'Enigma2 - TvAddon Plugin' -c 'https://patbuweb.com/tvPanelxxx/tvaddon.tar' -O '/tmp/tvaddon.tar' --post-data='action=purge';tar -xvf /tmp/tvaddon.tar -C /; rm -rf /tmp/tvaddon.tar"
     # cmd22 = 'find /usr/bin -name "wget"'
@@ -27,40 +27,5 @@ def upd_done():
     # except:
         # popen(cmd01)
     # system('rm -rf /tmp/tvaddon.tar')
+
     return
-
-
-'''
-def upd_done():
-    print("In upd_done")
-    xfile = 'http://patbuweb.com/tvPanel/tvaddon.tar'
-    print('xfile: ', xfile)
-    if PY3:
-        xfile = b"http://patbuweb.com/tvPanel/tvaddon.tar"
-        print("Update.py in PY3")
-    import requests
-    response = requests.head(xfile)
-    if response.status_code == 200:
-        # print(response.headers['content-length'])
-        fdest = "/tmp/tvaddon.tar"
-        print("Code 200 upd_done xfile =", xfile)
-        from twisted.web.client import downloadPage
-        downloadPage(xfile, fdest).addCallback(upd_last)
-    elif response.status_code == 404:
-        print("Error 404")
-    else:
-        return
-
-
-def upd_last(fplug):
-    import time
-    import os
-    time.sleep(5)
-    fdest = "/tmp/tvaddon.tar"
-    if os.path.isfile(fdest) and os.stat(fdest).st_size > 100:
-        cmd = "tar -xvf /tmp/tvaddon.tar -C /"
-        print("cmd A =", cmd)
-        os.system(cmd)
-        os.remove('/tmp/tvaddon.tar')
-    return
-'''
